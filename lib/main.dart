4ace3a4d634652a9e661a1c21f66f0bbfc2b0954
Sharad Shinde <shindesharad71@gmail.com>
2018-03-14 23:23:48 +0530

    new code from scratch

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,12 @@
-import 'dart:_http';
-import 'dart:convert';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new MaterialApp(
+  heome: new HomePage(),
+));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Network Requests',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Newtwork Request'),
-    );
-  }
+  HomePageState createState() => new HomePageState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var _counter;
-
-  void _incrementCounter() {
-    setState(() {
-      final url = 'https://jsonplaceholder.typicode.com/posts';
-      HttpRequest.request(url)
-      .then((value) {
-          _counter =  JSON.decode(value.responseText)['origin'];
-      })
-      .catchError((error) => print(error));
-    });
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ),
-    );
-  }
-}
+classs HomePageState
